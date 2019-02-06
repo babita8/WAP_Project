df556151ed8c93274e9a8eefa544309fb91c675e
Mai Tran <ttqmaikhtn@gmail.com>
2019-02-05 23:05:10 -0600

    Update to redirect to tasks.jsp

--- src/main/java/controller/TaskServlet.java
+++ src/main/java/controller/TaskServlet.java
@@ -38,7 +38,7 @@ public class TaskServlet extends HttpServlet {
         request.getSession().setAttribute("userInGroup", userInGroup);
 
         //request.getRequestDispatcher("tasks.jsp").forward(request, response);
-        response.sendRedirect("tasksEdit.jsp");
+        response.sendRedirect("tasks.jsp");
 
 
     }
